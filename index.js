8baed919a74ec97ef6aaa4dd593a42bca61683d5
zeus <zeus.ramirez347@gmail.com>
2021-05-19 11:44:00 -0400

    commit tree bb4f59e0f7362759b1d99a58abd7008ca64bf4ea

---
+++ index.js
@@ -0,0 +1,8 @@
+// function expression to divide 
+const divide = function(){
+     return 2000 / 100
+}
+// arrow function for square
+const square = x => x * x;
+// arrow function for add
+const add = (a , b) => a + b;
